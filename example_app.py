487a26286c769725283619cc56dca447762465a2
Grant Drew <grant.drew@xthreen.net>
2023-04-26 23:14:33 -0400

    progress bar, other rough progress 	modified:   assets/tesseract-logo-houndstoothed-alpha.ico     - cleaned up image edges alittle, still needs final touches 	modified:   example_app.py     - progress callback chain     - progress bar     - sleep task for testing / demoing

--- example_app.py
+++ example_app.py
@@ -1,17 +1,19 @@
 """example class hierarchy for a complex state machine application"""
 import threading
-import queue
 import time
+import queue
 import os
+import platform
 import gettext
 import locale
 import requests
 import PySimpleGUI as sg
+from abc import ABC, abstractmethod
 
 FILE_DIR = os.path.join(os.path.dirname(__file__), "i18n")
 FILE_NAME = os.path.split(__file__)[1].split(".")[0]
 
-# Set locale
+# Set initial locale
 locale.setlocale(locale.LC_ALL, "")
 
 gettext.bindtextdomain(FILE_NAME, FILE_DIR)
@@ -19,18 +21,6 @@ gettext.textdomain(FILE_NAME)
 _ = gettext.gettext
 
 sg.user_settings_filename(filename=FILE_NAME.join(".json"))
-
-
-# Fetch JSON data function
-def fetch_json(url):
-    """Fetch JSON data from the given URL"""
-    try:
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
-        json_data = response.json()
-        return json_data, None
-    except requests.exceptions.RequestException as fetch_error:
-        return None, str(fetch_error)
 
 
 # State classes
@@ -72,11 +62,15 @@ class State:
 
 class InitialState(State):
     """Initial state"""
+    def __init__(self, name, parent_machine):
+        super().__init__(name, parent_machine)
+        self.show_progress_bar = False
 
     def get_layout(self):
         # print("Getting layout for initial state")
         return [
             [sg.Text(_("Welcome to the complex state machine."))],
+            [sg.ProgressBar(100, orientation="h", size=(36, 20), key="-progress_bar-", visible=self.show_progress_bar)],
             [
                 sg.Button(_("Go to State A"), key="-go_to_state_a-"),
                 sg.Button(_("Go to State B"), key="-go_to_state_b-"),
@@ -104,7 +98,18 @@ class InitialState(State):
             if file_url:
                 next_state_name = "download"
                 data = file_url
+            else:
+                next_state_name = self.name
+        elif event == "-PROGRESS-":
+            progress = values["-PROGRESS-"]
+            if self.window:
+                self.window["-progress_bar-"].update(progress,visible=True)
+                if progress >= 100:
+                    self.window["-progress_bar-"].update(0, visible=False)
+
+            return None, None
         else:
+            print(f"something else, maybe unexpected: {event}")
             next_state_name = self.name
         return next_state_name, data
 
@@ -190,9 +195,7 @@ class StateC(State):
                 f"{self.name.capitalize()} State",
                 self.get_layout(),
                 no_titlebar=True,
-                grab_anywhere_using_control=True,
                 icon="assets/tesseract-logo-houndstoothed-alpha.ico",
-                titlebar_icon="assets/tesseract-logo-houndstoothed-alpha.ico",
                 relative_location=(128, 144),
                 finalize=True,
             )
@@ -209,41 +212,135 @@ class StateC(State):
         return next_state_name, data
 
 
-# DownloadManager class
+class HttpClient:
+    """HTTP client Singleton"""
+
+    _instance = None
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers.update(
+            {
+                "User-Agent": f"Learning Python (0.1.0) {platform.system()}/{platform.release()}",
+                "Accept-Language": locale.getlocale()[0],
+            },
+        )
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(HttpClient, cls).__new__(cls)
+            cls._instance.__init__()
+        return cls._instance
+
+    def get(self, url, timeout=5, **kwargs):
+        """Send a GET request"""
+        response = self.session.get(url, timeout=timeout, **kwargs)
+        response.raise_for_status()
+        return response
+
+    def post(self, url, timeout=5, **kwargs):
+        """Send a POST request"""
+        response = self.session.post(url, timeout=timeout, **kwargs)
+        response.raise_for_status()
+        return response
+
+    def close(self):
+        """Close the HTTP session"""
+        self.session.close()
+
+
+class Task(ABC):
+    """Abstract base class for tasks"""
+
+    def __init__(self, progress_callback=None):
+        self.progress_callback = progress_callback
+
+    @abstractmethod
+    def run(self):
+        """Run the task instance"""
+
+
+class SleepTask(Task):
+    """Sleep task class inherits from Task, used for unit tests"""
+
+    def __init__(self, seconds=4, progress_callback=None):
+        super().__init__(progress_callback)
+        self.seconds = seconds
+
+    def run(self):
+        print(f"Sleeping for {self.seconds} seconds")
+        start_time = time.time()
+        while True:
+            elapsed_time = time.time() - start_time
+            progress = (elapsed_time / self.seconds) * 100
+
+            self.progress_callback(progress)
+            if progress > 100:
+                break
+
+
+# class HttpGetTask(Task):
+#     """Request task class inherits from Task, used for HTTP requests"""
+
+#     def __init__(self, url, chunk_size=8192):
+#         self.http_client = HttpClient()
+#         self.url = url
+#         self.chunk_size = chunk_size
+#         self.content_length = 0
+#         self.content_loaded = 0
+
+#     def run(self):
+#         response = self.http_client.get(self.url)
+#         self.content_length = int(response.headers.get("Content-Length", 0))
+#         self.content_loaded = 0
+
+
 class DownloadManager:
     """Download manager / task queue class"""
 
-    def __init__(self):
+    def __init__(self, num_workers=2, progress_callback=None):
         self.task_queue = queue.Queue()
-        self.worker_thread = threading.Thread(target=self.worker, daemon=True)
-        self.worker_thread.start()
+        self.num_workers = num_workers
+        self.progress_callback = progress_callback
+
+    def set_progress_callback(self, progress_callback):
+        """Set the progress callback"""
+        self.progress_callback = progress_callback
+
+    def add_task(self, task):
+        """Add a task to the queue"""
+        print(f"Adding task {task}")
+        task.progress_callback = self.progress_callback  # set the callback
+        self.task_queue.put(task)
 
     def worker(self):
         """Worker thread function"""
         while True:
             task = self.task_queue.get()
+            print(f"Got task {task}")
             if task is None:
+                print("Got None, breaking")
                 break
-            self.download_file(task)
+            task.run()
             self.task_queue.task_done()
 
-    def download_file(self, file_url):
-        """Download the file at the given URL"""
-        print(f"Downloading file: {file_url}")
-        time.sleep(5)  # Simulate a long-running task
-        print(f"Download complete: {file_url}")
-        return True
+    def start(self):
+        """Start a new worker thread"""
+        threads = []
+        for _ in range(self.num_workers):
+            thread = threading.Thread(target=self.worker, daemon=True)
+            thread.start()
+            threads.append(thread)
+            print(f"Started worker thread {thread.name}")
 
-    def add_task(self, file_url):
-        """Add a task to the queue"""
-        self.task_queue.put(file_url)
+        return threads  # return the worker threads
 
     def stop(self):
-        """Stop the worker thread"""
-        self.task_queue.put(None)
+        """Stop all worker threads"""
+        for _ in range(self.num_workers):
+            self.task_queue.put(None)
 
 
-# StateMachine class
 class StateMachine:
     """State machine class"""
 
@@ -266,6 +363,13 @@ class StateMachine:
         """Run the state machine"""
         self.current_state.open_window()
         secondary_windows = []  # Secondary window container list
+        self.download_manager.set_progress_callback(
+            lambda progress: self.current_state.window.write_event_value(
+                "-PROGRESS-", progress
+            )
+            if self.current_state.window is not None else None
+        )
+        worker_threads = self.download_manager.start()
         while True:
             # Read the from all open windows
             window, event, values = sg.read_all_windows()
@@ -283,7 +387,9 @@ class StateMachine:
 
                 # Check if the next state name is "download"
                 if next_state_name == "download":
-                    self.download_manager.add_task(data)
+                    print(f"Data: {data}")
+                    try_task = SleepTask(10)
+                    self.download_manager.add_task(try_task)
                 elif next_state_name == "state_c":
                     secondary_windows.append(self.states[next_state_name])
                     self.states[next_state_name].open_window()
@@ -295,13 +401,17 @@ class StateMachine:
             elif any(window == state.window for state in secondary_windows):
                 for secondary_window in secondary_windows[:]:
                     if window == secondary_window.window:
-                        next_state_name, _ = secondary_window.transition_state(event, values)
+                        next_state_name, _ = secondary_window.transition_state(
+                            event, values
+                        )
                         if next_state_name is None:
                             secondary_window.close_window()
                             secondary_windows.remove(secondary_window)
                             break
 
         self.download_manager.stop()
+        for thread in worker_threads:
+            thread.join()
 
 
 if __name__ == "__main__":
